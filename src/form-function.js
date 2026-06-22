841d2e862b8702714f1fba77f25acfb63ec38ece
Yarik <urik7k2@gmail.com>
2026-06-22 16:00:24 +0300

    Fixed Errors

--- src/form-function.js
+++ src/form-function.js
@@ -9,20 +9,23 @@ const createError = (text, input) => {
 
 divFormElemInfo.addEventListener("input", () => {
     const regExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    const regExpForMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     document.querySelectorAll(".error").forEach((el) => el.remove());
 
-    if (!regExpForMail.test(email.value)) {
-        createError("Please enter a valid email address.", email);
-    }
-
-    if (!regExp.test(password.value) || password.value === '') {
+    if (!regExp.test(password.value) || password.value === "") {
         createError(
             "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.",
-            password
+            password,
         );
     } else if (passwordConfirm.value !== password.value) {
         createError("Passwords must match", passwordConfirm);
+    }
+});
+
+email.addEventListener("click", () => {
+    const regExpForMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regExpForMail.test(email.value)) {
+        createError("Please enter a valid email address.", email);
     }
 });
